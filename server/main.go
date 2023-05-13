feb5b6e66fda8dac64e2ae1ac9333b097db9eb74
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-05-13 20:55:52 +0300

    Update go server for updated proto file

--- server/main.go
+++ server/main.go
@@ -2,32 +2,48 @@ package main
 
 import (
 	"context"
+	pb "ficsithub/server/pb/ficsithub"
+	"log"
 	"net"
-	"fmt"
-	"bending/api"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+type server struct {
+	pb.UnimplementedFicsitHubServer
+}
+
+func (s *server) GetAllPioneers(ctx context.Context, in *pb.Empty) (*pb.PioneersList, error) {
+	log.Printf("Received request: %v", in.ProtoReflect().Descriptor().FullName())
+	return &pb.PioneersList{
+		Pioneers: getSamplePioneers(),
+	}, nil
+}
+
 func main() {
-	lis, err := net.Listen("tcp", "localhost:8080")
+	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		panic(err)
 	}
-	grpcServer := grpc.NewServer()
-	api.RegisterBendingServiceServer(grpcServer, &BendingService{})
-	fmt.Println("Starting server...")
-	grpcServer.Serve(lis)
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterFicsitHubServer(s, &server{})
+	log.Printf("Server started running on port 50051")
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
-type BendingService struct {
-	api.UnimplementedBendingServiceServer
-}
-
-func (s *BendingService) CalculateBendingType(ctx context.Context,
-				req *api.BendingRequest) (*api.BendingResponse, error) {
-	BendingTypes := []string{"Air", "Water", "Earth", "Fire"}
-	return &api.BendingResponse{
-		BendingType: BendingTypes[req.BirthYear % 4],
-	}, nil
+func getSamplePioneers() []*pb.Pioneer {
+	samplePioneers := []*pb.Pioneer{
+		{
+			Username: "emingbt",
+		},
+		{
+			Username: "hegobin",
+		},
+	}
+	return samplePioneers
 }
